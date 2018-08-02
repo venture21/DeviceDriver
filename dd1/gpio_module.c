d0fc69ad1c2f10b7520292f577508fd8fcf614ed
venture21 <park0122@gmail.com>
2018-08-02 14:22:32 +0900

    add gpio.c

--- dd1/gpio_module.c
+++ dd1/gpio_module.c
@@ -33,17 +33,59 @@ MODULE_DESCRIPTION("RASPBERRY PI GPIO LED DRIVER");
 #define GPIO_DEVICE "gpioled"
 #define GPIO_LED		17	// Raspi GPIO17 -> LED
 
+#define STR_SIZE 100
+
+static char msg[STR_SIZE] = {0};
+
 struct cdev gpio_cdev;
 
 static struct file_operations gpio_fops = {
 		.owner = THIS_MODULE,
-	//	.read   = gpio_read,
-	//	.write = gpio_write,
-	//	.open   = gpio_open,
-	//	.release = gpio_close,
+		.read   = gpio_read,
+		.write = gpio_write,
+		.open   = gpio_open,
+		.release = gpio_close,
 };
 
 volatile unsigned int *gpio;
+
+static int gpio_open(struct inode *inod, struct file *fil)
+{
+	// app에서 open()가 호출될 때마다 모듈의 사용 카운터를 증가 시킨다.
+	try_module_get(THIS_MODULE);
+	printk(KERN_INFO "GPIO Device opened\n");
+	return 0;
+	}
+
+static int gpio_close(struct inode *inod, struct file *fil)
+{
+	// app에서 close()함수가 호출될 때마다 모듈의 사용 카운터를 감소 시킨다.
+	module_put(THIS_MODULE);
+	printk(KERN_INFO "GPIO Device closed\n");
+	return 0;
+}
+
+static ssize_t gpio_read(struct file *inode, const char *buff, size_t len, loff_t *off)
+{
+	// app에서 read()함수가 호출될 때마다 gpio_read()함수가 호출된다.
+	int count;
+	strcat(msg, "from kernel);
+	count = copy_to_user(buff, msg, strlen(msg)+1);
+
+	printk(KERN_INFO "GPIO Device read:%s\n", msg);
+	return (ssize_t)count;
+}
+
+static ssize_t gpio_write(struct file *inode, const char *buff, size_t len, loff_t *off)
+{
+	int count;
+	memset(msg, 0, STR_SIZE);
+	count = copy_from_user(msg,buff,len);
+	(strcmp(msg,"0"))?GPIO_CLR(GPIO_LED):GPIO_SET(GPIO_LED);
+	printk(KERN_INFO "GPIO Device write : $s\n", msg);
+	
+	return (ssize_t) count;
+}
 
 int initModule(void)
 {
@@ -93,7 +135,20 @@ int initModule(void)
 
 void cleanupModule(void)
 {
-	printk(KERN_INFO "called : cleanupModule()\n");
+	dev_t devno;
+	devno = MKDEV(GPIO_MAJOR, GPIO_MINOR);
+	
+	// 1. 문자 디바이스의 등록을 해제한다.
+	unregister_chrdev_region(devno, 1);
+	
+	// 2. 문자 디바이스의 구조체를 삭제한다.
+	cdev_del(&gpio_cdev);
+	
+	// 3. GPIO unmap;
+	if(gpio)
+		iounmap(gpio);
+		
+	printk(KERN_INFO "operation is done! : cleanupModule()\n");		
 }
 
 
